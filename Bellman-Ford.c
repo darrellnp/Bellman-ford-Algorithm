507744ee28d6bb477de29388317e82a013b45078
darrellnp <102579627+darrellnp@users.noreply.github.com>
2023-05-28 20:40:16 +0800

    Update Bellman-Ford.c

--- Bellman-Ford.c
+++ Bellman-Ford.c
@@ -45,7 +45,7 @@ void FinalSolution(int dist, int n)
     int i;
  
     for (i = 0; i < n; ++i){
-printf("%d \t\t %f\n", i, dist[i]);
+printf("%d \t\t %d\n", i, dist[i]);
 
 }
  
@@ -70,7 +70,7 @@ void BellmanFord(struct Graph* graph, int source)
     //The shortest path of graph that contain V vertices, never contain "V-1" edges. So we do here "V-1" relaxations
     for (i = 1; i <= V-1; i++)
     {
-        for (j = 0; j < E; j--)
+        for (j = 0; j < E; j++)
         {
             int u = graph->edge[j].source;
  
